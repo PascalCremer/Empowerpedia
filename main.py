0cd0da7043f5626e7ab02692b83251b10e958f02
Henning Becker <h.becker@oedenstockach.de>
2014-08-29 11:07:02 +0200

    SQL implementation. first commit.

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from sets import Set
+from itertools import chain
+import MySQLdb
+db=MySQLdb.connect(host="172.27.1.184", user="empowerpedia", passwd="empowerpedia", db="wikipedia")
 
 def children(node):
-	if node <= 3:
-		return [node*2+1, node*2+2]
-	elif node == 4:
-		return [9]
-	else:
-		return []
+	cursor = db.cursor()
+	cursor.execute("SELECT page.page_id as children from pagelinks inner join page on pagelinks.pl_title = page.page_title and pagelinks.pl_namespace = page.page_namespace WHERE pagelinks.pl_from_namespace = 0 AND pagelinks.pl_from = %s" % node)
+
+	data = cursor.fetchall()
+	data = list(chain(*data))
+	return data
+
+def name(node)
+	cursor = db.cursor()
+	cursor.execute("SELECT page.page_name FROM page WHERE page.page_id = %s" % node)
+	data = cursor.fetchall()
+	return data[0]
 
 def empowerment(node, depth, visited):
 	queue = []
@@ -21,7 +30,8 @@ def empowerment(node, depth, visited):
 	return len(visited)
 
 empValues = []
-nodes = range(10)
+#nodes = range(10)
+nodes = [5982813]
 for node in nodes:
 	visited = Set([])
 	value = empowerment(node, 3, visited)
