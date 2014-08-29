7f809b44284ae8cfe79f2b6b11db8936af9b77ec
Henning Becker <h.becker@oedenstockach.de>
2014-08-29 12:08:31 +0200

    Added nodes function.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from sets import Set
 from itertools import chain
+import random
+import math
 import MySQLdb
+
 db=MySQLdb.connect(host="172.27.1.184", user="empowerpedia", passwd="empowerpedia", db="wikipedia")
+
+def nodes(offset):
+	cursor = db.cursor()
+	cursor.execute("SELECT page.page_id FROM page WHERE page_namespace = 0 LIMIT %s,10000" % offset)
+	return list(chain(*cursor.fetchall()))
 
 def children(node):
 	cursor = db.cursor()
@@ -13,11 +21,21 @@ def children(node):
 
 def name(node):
 	cursor = db.cursor()
-	cursor.execute("SELECT page.page_name FROM page WHERE page.page_id = %s" % node)
+	cursor.execute("SELECT page.page_title FROM page WHERE page.page_id = %s" % node)
 	data = cursor.fetchall()
-	return data[0]
+	return data[0][0]
 
-def empowerment(node, depth, visited):
+def print_child_names(node):
+	print name(node)
+	namelist = []
+	for child in children(node):
+		namelist.append(name(child))
+	print namelist
+
+
+
+def empowerment(node, depth):
+	number = 0
 	queue = []
 	for child in children(node):
 		if child not in visited:
